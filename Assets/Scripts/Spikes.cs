83c49922ecc103578a5e6309f3cbceeaee4626c8
KambellT <kambellt@yahoo.com>
2018-11-16 15:37:44 +0000

    Created levels

---
+++ Assets/Scripts/Spikes.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spikes : MonoBehaviour {
+
+    // unity calls this function automatically
+    // when our spikes touch any other object
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+
+        // check if the thing we collided with
+        // is the player (aka has a Player script)
+        Player playerScript = collision.collider.GetComponent<Player>();
+
+        // only do something if the thing we ran into
+        // was in fact the player
+        // aka playerScript is not null
+        if (playerScript != null)
+        {
+            // we DID hit the player!
+
+            // KILL THEM
+            playerScript.Kill();
+        }
+
+    }
+
+}
