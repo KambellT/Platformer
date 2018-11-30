e6d44bfdcf791e6e711063e68664d755a90e892a
KambellT <kambellt@yahoo.com>
2018-11-30 14:29:35 +0000

    Added a game over screen

---
+++ Assets/Scripts/TitleButton.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+// this allows us to use the scene loading function
+using UnityEngine.SceneManagement;
+public class TitleButton : MonoBehaviour {
+
+    // called when title button is clicked
+	public void GoToTitle()
+    {
+        // return to title scene
+        SceneManager.LoadScene("TitleScreen");
+    }
+}
